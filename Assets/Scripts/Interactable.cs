96bf0132bc0b36a0087a57ba24b23db87ce2f0bd
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2023-03-25 13:48:13 +0000

    Interaction script to interact with objects by clicking them
    When an interaction object is clicked the player will move towards it and once its less than 2f to the object it will trigger the objects interaction

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    public void Interact()
+    {
+        //Create an interaction with anything that we need here
+        Debug.Log("Interacted with item");
+    }
+}
--- Assets/Scripts/PointAndClickMovement.cs
+++ Assets/Scripts/PointAndClickMovement.cs
@@ -14,6 +14,8 @@ public class PointAndClickMovement : MonoBehaviour
     private Camera _camera;
     private Vector3 _currentGoToPos;
     private NavMeshAgent _navMeshAgent;
+    private bool _bInteracting = false;
+    private Interactable _interactable;
     private void Start()
     {
         _camera = Camera.main;
@@ -24,18 +26,49 @@ public class PointAndClickMovement : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            _navMeshAgent.isStopped = false;
+            
             Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, maxDistance, groundLayer))
+            if (Physics.Raycast(ray, out hit, maxDistance))
             {
-                _currentGoToPos = hit.point;
-                DrawLocation();
-                MoveToLocation();
+                if (hit.transform.CompareTag("Interactable"))
+                {
+                    _interactable = hit.transform.gameObject.GetComponent<Interactable>();
+                    _bInteracting = true;
+                    
+                    RaycastHit newHit;
+                    
+                    if (Physics.Raycast(hit.point + ray.direction * 0.01f, ray.direction, out newHit, maxDistance, groundLayer))
+                    {
+                        _currentGoToPos = newHit.point;
+                        DrawLocation();
+                        MoveToLocation();
+                    }
+                }
+                else if (hit.transform.CompareTag("Ground"))
+                {
+                    _currentGoToPos = hit.point;
+                    DrawLocation();
+                    MoveToLocation();
+                }
             }
         }
-    }
 
+        if (_bInteracting)
+        {
+            //Debug.Log(Vector3.Distance(transform.position, _currentGoToPos));
+            if (Vector3.Distance(transform.position, _currentGoToPos) < 2f && _interactable)
+            {
+                _navMeshAgent.isStopped = true;
+                _interactable.Interact();
+                _bInteracting = false;
+            }
+        }
+        
+    }
+    
     private void MoveToLocation()
     {
         _navMeshAgent.SetDestination(_currentGoToPos);
