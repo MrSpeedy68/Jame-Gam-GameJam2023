f961c99d8f475876c3537c44c1c685a3f743710d
UppedMagi <spiritsmonarch@gmail.com>
2023-03-26 22:32:05 +0100

    HagUIStart

---
+++ Assets/Scripts/CardSelector.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSelection : MonoBehaviour
+{
+    [SerializeField]private List<GameObject> cardList; // List of existing cards
+    [SerializeField]private List<Button> buttonList; // List of buttons to assign the selected cards to
+    [SerializeField]private int pp;
+
+    private List<GameObject> selectedCards = new List<GameObject>(); // List of selected cards
+
+    void Start()
+    {
+        SelectCards();
+
+        // Assign the selected cards to the buttons
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            buttonList[i].image.sprite = selectedCards[i].GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+
+    void SelectCards()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            int randomIndex = Random.Range(0, cardList.Count); // Get a random index from the list of cards
+
+            if (!selectedCards.Contains(cardList[randomIndex]))
+            {
+                selectedCards.Add(cardList[randomIndex]); // Add the card to the list of selected cards
+            }
+            else
+            {
+                i--; // If the card has already been selected, try again
+            }
+        }
+    }
+}
